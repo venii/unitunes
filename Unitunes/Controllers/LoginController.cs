6753d0653a94a6927dace3a5349fa2c3eaa5f701
veni <viniciusferreirawk@gmail.com>
2016-06-15 23:30:16 -0300

    login feito

--- Unitunes/Controllers/LoginController.cs
+++ Unitunes/Controllers/LoginController.cs
@@ -27,78 +27,63 @@ namespace Unitunes.Controllers
         [HttpPost]
         public ActionResult VerifyLogin()
         {
-            //Unitunes.Models.Academico;   
-            //ModelState
-            //Unitunes.Models.dbEntities.
-
-            //var login = from Academico where academico.Email = Request['login'];
-            //IEnumerable<Academico> resultado1 = from e in fonteDados
-            //                                 where e.Cor == "vermelha"
-            //                                 select e.Nome;
 
             var ctx = new dbEntities();
             
             var login = ctx.AcademicoSet;
             String requestU = Request["login"];
             String requestP = Request["password"];
-            
-            //foreach de dados vindo do loginDb(dbcontext)
-            //linq
-            var existeUsuario = from u in login where u.Email == requestU & u.Password == requestP  select u;
 
-            if (existeUsuario.Count() == 0)
+            Academico novoLogin = new Academico();
+            novoLogin.Email = requestU;
+            novoLogin.Password = requestP;
+
+            if (Unitunes.Models.ModelosApp.Academico.isAcademicoExists(novoLogin))
             {
+                return Redirect("/Login/Principal");
 
-                
-                    Academico a = new Academico();
-                    a.Password = requestP;
-                    a.Email = requestU;
-                    a.PrimeiroNome = "juca";
-                    a.SegundoNome = "silva";
-
-                    ctx.AcademicoSet.Add(a);
-                    ctx.SaveChanges();
-
-
-                    ViewBag.login = "nao existe";
-                    ViewBag.password = "nao existe";
-                
             }
             else
             {
-                ViewBag.login = "existe";
-                ViewBag.password = "existe";
+                ModelState.AddModelError("error", "Usuario não existe");
             }
-            //lambda
-
 
             return View();
 
         }
-        
-        [HttpPost]
+
+        [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
         public ActionResult Registrar(Academico academico)
         {
             if (HttpContext.Request.HttpMethod == "POST")
             {
                 //verifica se existe o registro
                 //quando enviar o post
-                var ctx = new dbEntities();
-            
 
-                var academicos = ctx.AcademicoSet;
-                var existeUsuario = from u in academicos where u.Email == academico.Email & u.Password == academico.Password select u;
-
-                if (existeUsuario.Count() == 0)
+                if (academico.Email != null && academico.Password != null)
                 {
-                    ctx.AcademicoSet.Add(academico);
-                    ctx.SaveChanges();
-                }
-                else
-                {
-                    ModelState.AddModelError("Erro:", "Usuario Já existe");
-                }
 
+                    var ctx = new dbEntities();
+                    var academicos = ctx.AcademicoSet;
+                    
+                    
+                    //verifica se usuario nao existe
+                    if (!Unitunes.Models.ModelosApp.Academico.isAcademicoExists(academico))
+                    {
+                        
+                            //cria usuario
+                            Unitunes.Models.ModelosApp.Academico.adicionarAcademico(academico);
+                            return Redirect("/Login/Principal");
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("error", "Usuario Já existe");
+                    }
+                }
+                else { 
+                    ModelState.AddModelError("error", "Preencha o formulario corretamente");
+                }
                 // The action is a POST.
             }
             return View();
---
+++ Unitunes/Models/ModelosApp/Academico.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Unitunes.Models.ModelosApp
+{
+    public class Academico
+    {
+        public static bool adicionarAcademico(Unitunes.Models.Academico academico)
+        {
+            var ctx = new dbEntities();
+            
+            try { 
+                ctx.AcademicoSet.Add(academico);
+                ctx.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool isAcademicoExists(Unitunes.Models.Academico academico)
+        {
+            var ctx = new dbEntities();
+            var login = ctx.AcademicoSet;
+
+            var existeUsuario = from u in login where u.Email == academico.Email & u.Password == academico.Password select u;
+
+            if (existeUsuario.Count() > 0)
+            {
+                return true;
+
+            }
+
+            return false;
+        }
+ 
+    }
+
+
+}
