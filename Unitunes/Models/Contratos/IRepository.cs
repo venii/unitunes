a7cf1c6a6af6aed212eee069bb93ac75891f772b
veni <viniciusferreirawk@gmail.com>
2016-06-18 18:38:06 -0300

    git all

---
+++ Unitunes/Models/Contratos/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unitunes.Models.Contratos
+{
+    interface IRepository<T> where T : class
+    {
+         T GetById(int id);
+         IQueryable<T> GetAll(Expression<Func<T, bool>> filter);
+         bool Save(T entity);
+         bool Update(T entity);
+         bool Delete(int id);
+         bool Delete(T entity);
+    }
+}
---
+++ Unitunes/Models/Validacoes/Media.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Unitunes.Models
+{
+    [MetadataType(typeof(MediaMetaData))]
+    public partial class Media
+    {
+        /*adiciona validacao de MediaMetada em Media partial*/
+    }
+
+    public class MediaMetaData
+    {
+        [Required(ErrorMessage = "Nome is required")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Nome is required")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Preco is required")]
+        public decimal Preco { get; set; }
+        [Required(ErrorMessage = "Categoria is required")]
+        public string Categoria { get; set; }
+
+    }
+}
